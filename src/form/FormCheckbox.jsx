dcb76068b86fac83fe583b1e9b6bf463fca456dd
frecape <frecape@lifesquare.fr>
2024-04-14 16:09:28 +0200

    Fix migration + prepare lint

--- src/form/FormCheckbox.jsx
+++ src/form/FormCheckbox.jsx
@@ -1,31 +1,40 @@
-import { Checkbox, FormControl, FormControlLabel, Typography } from "@mui/material"
-import { Controller, useController } from "react-hook-form"
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+} from "@mui/material";
+import { useController } from "react-hook-form";
+import { errorMessage } from "./formUtils";
 
-export const FormCheckbox = ({ name, label, control, rules, ...props }) => {
+export const FormCheckbox = ({ name, label, control, rules,}) => {
+  const {
+    field: { onChange, value, onBlur, ref },
+    fieldState: { error },
+    formState: { isSubmitting },
+  } = useController({
+    name,
+    control,
+    rules: rules,
+    defaultValue: false,
+  });
 
-
-
-
-    const {
-        field: { onChange, value, onBlur, ref },
-        fieldState: { invalid, error },
-        formState: { isSubmitting }
-    } = useController({
-        name,
-        control,
-        rules: rules,
-        defaultValue: false
-    })
-
-    return (
-        <FormControl fullWidth>
-            <FormControlLabel control={<Checkbox checked={Boolean(value)}
-                color="primary"
-                onChange={onChange}
-                onBlur={onBlur}
-            />}
-                label={label}
-            />
-        </FormControl>
-    )
-}
+  return (
+    <FormControl fullWidth>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={Boolean(value)}
+            color="primary"
+            onChange={onChange}
+            onBlur={onBlur}
+            inputRef={ref}
+          />
+        }
+        disabled={isSubmitting}
+        label={label}
+      />
+      <FormHelperText error>{errorMessage(error)}</FormHelperText>
+    </FormControl>
+  );
+};
