45d40fe882ad3dccceeac619c19770429635fd47
frecape <florian.recape@wanadoo.fr>
2024-04-18 17:59:53 +0200

    Formatting

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,80 +1,83 @@
 import { AppBar, Drawer, IconButton, Toolbar, Typography } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import { NavigationBar } from "./NavigationBar";
 import { useState } from "react";
 import { Box } from "@mui/system";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
 import { useAuth } from "../hooks/useAuth";
 import { Navigate, useNavigate } from "react-router-dom";
 
 export const AuthLayout = ({ children }) => {
+  const drawerWidth = 240;
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const navigate = useNavigate();
 
+  const { user, logout } = useAuth();
 
-    const drawerWidth = 240;
-    const [openDrawer, setOpenDrawer] = useState(false);
-    const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
 
-    const {user, logout} = useAuth();
+  if (!user) return <Navigate to="/login" />;
 
-    const handleLogout = () => {
-        logout();
-        navigate('/login');
-    }
+  return (
+    <div>
+      <AppBar
+        position="fixed"
+        sx={{
+          bgcolor: "secondary.light",
+          color: "primary",
+        }}
+        enableColorOnDark
+      >
+        <Toolbar>
+          <IconButton
+            aria-label="open drawer"
+            onClick={() => setOpenDrawer(true)}
+            color="inherit"
+            edge="start"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Recoco
+          </Typography>
+          <IconButton color="inherit">
+            <AccountCircleIcon />
+          </IconButton>
+          <IconButton color="inherit" onClick={handleLogout}>
+            <PowerSettingsNewIcon />
+          </IconButton>
+        </Toolbar>
+        <Drawer
+          sx={{
+            width: 200,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={openDrawer}
+        >
+          <NavigationBar onClose={() => setOpenDrawer(false)} />
+        </Drawer>
+      </AppBar>
 
-    if(!user)
-        return <Navigate to="/login" />;
-
-
-    return (
-        <div>
-            <AppBar position="fixed"
-                sx={{
-                    bgColor: 'background.default',
-                    color: 'primary'
-                }} enableColorOnDark>
-                <Toolbar>
-                    <IconButton
-                        aria-label="open drawer"
-                        onClick={() => setOpenDrawer(true)}
-                        color="inherit"
-                        edge="start">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Recoco
-                    </Typography>
-                    <IconButton color="inherit">
-                        <AccountCircleIcon />
-                    </IconButton>
-                    <IconButton color="inherit" onClick={handleLogout}>
-                        <PowerSettingsNewIcon />
-                    </IconButton>
-                </Toolbar>
-                <Drawer
-                    sx={{
-                        width: 200,
-                        flexShrink: 0,
-                        '& .MuiDrawer-paper': {
-                            width: drawerWidth,
-                            boxSizing: 'border-box',
-                        },
-                    }}
-                    variant="persistent"
-                    anchor="left"
-                    open={openDrawer}>
-                    <NavigationBar onClose={() => setOpenDrawer(false)} />
-                </Drawer>
-            </AppBar>
-
-            <Box component="main"
-                sx={{
-                    flexGrow: 1,
-                    marginLeft: openDrawer ? `${drawerWidth}px` : '0px',
-                }}>
-                <Toolbar />
-                {children}
-            </Box>
-        </div>
-    )
-}
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          marginLeft: openDrawer ? `${drawerWidth}px` : "0px",
+        }}
+      >
+        <Toolbar />
+        {children}
+      </Box>
+    </div>
+  );
+};
